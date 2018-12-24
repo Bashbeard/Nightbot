d9f4112f748483908f25bcfda2dec0f43fd5e5ea
Bashbeard <46112437+Bashbeard@users.noreply.github.com>
2018-12-24 15:15:22 -0500

    Create question.php

---
+++ question.php
@@ -0,0 +1,31 @@
+<?php
+$num = rand ( 0 , 23);
+$questionsArray = array(
+"If you didn’t have to sleep, what would you do with the extra time?",
+"What’s your favorite piece of clothing you own / owned?",
+"What hobby would you get into if time and money weren’t an issue?",
+"What would your perfect room look like?",
+"How often do you play sports?",
+"What fictional place would you most like to go?",
+"What job would you be terrible at?",
+"When was the last time you climbed a tree?",
+"If you could turn any activity into an Olympic sport, what would you have a good chance at winning medal for?",
+"What is the most annoying habit that other people have?",
+"What job do you think you’d be really good at?",
+"What skill would you like to master?",
+"What would be the most amazing adventure to go on?",
+"If you had unlimited funds to build a house that you would live in for the rest of your life, what would the finished house be like?",
+"What’s your favorite drink?",
+"What state or country do you never want to go back to?",
+"What songs have you completely memorized?",
+"What game or movie universe would you most like to live in?",
+"What do you consider to be your best find?",
+"Are you usually early or late?",
+"What pets did you have while you were growing up?",
+"When people come to you for help, what do they usually want help with?",
+"What takes up too much of your time?",
+"What do you wish you knew more about?");
+
+echo $questionsArray[num];
+
+?> 
